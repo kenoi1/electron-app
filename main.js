568a666abd9dd7f58a67685eafdbbf09101abc3b
kenoi <derekhongdalin@gmail.com>
2023-09-13 22:36:48 -0400

    I added a bar
    it is ugly

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron') 
+// app - controls app event lifecycle, BrowserWindow - create and manage app windows
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,6 +10,18 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
+
+app.whenReady().then(() => { // opens a window if none are open (macOS)
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
+
+app.on('window-all-close', () => { // closes app when all windows are closed (linux/windows)
+  if (process.platform !=='darwin') app.quit()
+})
+
+
+   
