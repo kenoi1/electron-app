92a127fab5132e4d5ef915a22915ced9c0934231
kenoi <derekhongdalin@gmail.com>
2023-10-18 01:29:51 -0400

    audio
    UWU

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const { app, BrowserWindow } = require('electron')
 // app - controls app event lifecycle, BrowserWindow - create and manage app windows
 
 const createWindow = () => {
+  var path = require('path');
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: path.join(__dirname, "icon2crop.jpg")
   })
 
   win.loadFile('index.html')
